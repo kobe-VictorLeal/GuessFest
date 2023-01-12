571c07b3ec10cbe4acd37f148eb74f1e89f2fb82
Victor Leal <victor.leal@kobe.io>
2023-01-11 21:08:45 -0300

    theme row sets

--- lib/menu/components/themes_row_widget.dart
+++ lib/menu/components/themes_row_widget.dart
@@ -5,28 +5,58 @@ class ThemesRowWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Column(
-        children: [
-          const Text("Dalee"),
-          Container(
-            padding: const EdgeInsets.only(left: 15, top: 15, right: 15, bottom: 15),
-            child: Image.asset('assets/images/menu/bars/greenBar.png'),
+    return Column(
+      children: [
+        const Text(
+          "GERALGERALGERAL",
+          style: TextStyle(
+            fontFamily: "Bebas",
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.normal,
           ),
-          Container(
-            height: 200,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: themes.length,
-                itemBuilder: (ctx, index) {
-                  return Container(
-                    width: 200,
-                    child: Image.asset('assets/images/menu/themes/action.png'),
-                  );
-                }),
-          )
-        ],
-      ),
+          textAlign: TextAlign.center,
+        ),
+        Container(
+            padding: const EdgeInsets.only(left: 30, top: 15, right: 15, bottom: 30),
+            height: 120,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/menu/bars/greenBar.png',
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.fill,
+                ),
+                const Center(
+                  child: Text(
+                    "GERAL",
+                    style: TextStyle(
+                      fontFamily: "Bebas",
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                      decoration: TextDecoration.none,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            )),
+        SizedBox(
+          height: 150,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: themes.length,
+              itemBuilder: (ctx, index) {
+                return Container(
+                  padding: const EdgeInsets.only(left: 5, top: 5, right: 5, bottom: 5),
+                  width: 150,
+                  child: Image.asset('assets/images/menu/themes/action.png'),
+                );
+              }),
+        )
+      ],
     );
   }
 }
