2d1411d2a3fe74af3faf8ef26ce038297b0f0153
Victor Leal <victor.leal@kobe.io>
2023-03-16 19:39:52 -0300

    popup menu sets

--- lib/menu/components/menu_popup_widget.dart
+++ lib/menu/components/menu_popup_widget.dart
@@ -5,24 +5,28 @@ class MenuPopupWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(30),
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      alignment: Alignment.center,
-      child: Stack(
-        children: [
-          Image.asset(
-            'assets/images/menu/popup/popupTheme.png',
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.fill,
-          ),
-          ElevatedButton(
-              child: Text("Fechar", style: TextStyle(fontSize: 30)),
+    return Material(
+      child: Container(
+        padding: const EdgeInsets.all(30),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.center,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/menu/popup/popupTheme.png',
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.fill,
+            ),
+            IconButton(
+              icon: Image.asset('assets/images/menu/buttons/exit.png'),
+              iconSize: 50,
               onPressed: () {
                 Navigator.pop(context);
-              }), //function
-        ],
+              },
+            )
+          ],
+        ),
       ),
     );
   }
